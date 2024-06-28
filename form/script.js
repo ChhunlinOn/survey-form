b21ba9bf1794697f8bbbabf889cc617cd517a348
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-06-28 10:33:24 +0700

    done post

--- form/script.js
+++ form/script.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent the form from submitting the traditional way (page reload)
 
     let Name = document.getElementById('name').value;
-    let age = document.getElementById('email').value;
+    let email = document.getElementById('email').value;
     let grade = document.querySelector('input[type="radio"][name="grade"]:checked').value;
     let school = document.querySelector('input[type="radio"][name="school"]:checked').value;
     let feeling = document.querySelector('input[type="radio"][name="feeling"]:checked').value;
@@ -21,6 +21,7 @@ form.addEventListener("submit", function(event) {
     let Question20 = document.querySelector('input[type="text"][name="Q20"]').value;
     let Question21 = document.querySelector('input[type="text"][name="Q21"]').value;
     let Question22 = document.querySelector('input[type="text"][name="Q22"]').value;
+
 
     //all checkbox with textbox
     // Get all checked checkboxes with name "skill-interested"
@@ -53,7 +54,7 @@ form.addEventListener("submit", function(event) {
 
     // Log values to the console
     console.log('Name: ', Name);
-    console.log('Age: ', age);
+    console.log('Age: ', email);
     console.log('Grade: ', grade);
     console.log('School: ', school);
     console.log('feel: ',feeling);
@@ -73,6 +74,70 @@ form.addEventListener("submit", function(event) {
     console.log('Question20: ',Question20);
     console.log('Question21: ',Question21);
     console.log('Question22: ',Question22);
+
+
+
+
+    //post api
+    let dataValue = [];
+
+  for (let i = 1; i <= 21; i++) {
+    dataValue.push({
+      email: email,
+      fullname: Name,
+      question_id: i + 0,
+      answer: [""],
+      created_by: "group7",
+    });
+  }
+  console.log(dataValue);
+  dataValue[0].answer = [Name]
+  dataValue[1].answer = [email]
+  dataValue[2].answer = [grade]
+  dataValue[3].answer = [school]
+  dataValue[4].answer = [feeling]
+  dataValue[5].answer = [YesNOQuestion7]
+  dataValue[6].answer = [Question8]
+  dataValue[7].answer = [selectedSkillsValue]
+  dataValue[8].answer = [Question10]
+  dataValue[9].answer = [Question11]
+  dataValue[10].answer = [Question12]
+  dataValue[11].answer = [Question13]
+  dataValue[12].answer = [prefer_To_learn]
+  dataValue[13].answer = [learning_environment]
+  dataValue[14].answer = [Question16]
+  dataValue[15].answer = [Question17]
+  dataValue[16].answer = [learn]
+  dataValue[17].answer = [Question19]
+  dataValue[18].answer = [Question20]
+  dataValue[19].answer = [Question21]
+  dataValue[20].answer = [Question22]
+
+  function postApi() {
+    fetch("https://wmad-survey-backend.vercel.app/api/form-answers/submit", {
+      method: "POST",
+      body: JSON.stringify({ answers: dataValue }),
+      headers: {
+        "Content-Type": "application/json",
+        "x-secret-key":
+          "3c9e1f0a4d0f4e9b8b1a2e0d4e3f5a6c7d8e9f0a1b2c3d4e5f6a7b8c9d0e1f2",
+      },
+    })
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Error: Invalid data");
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  }
+
+  postApi();
 });
 // Separate event listener for the change event of the checkbox
 
@@ -147,4 +212,6 @@ form.addEventListener("submit", function(event) {
             enviroment_radio.value = this.value;
         }
     })
+
+
 
